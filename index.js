1ca33d16d6e8921af41f6f45fa3254348842ee3f
Prajeetha313 <prajeetharajkumar1042@gmail.com>
2022-07-16 20:16:06 +0530

    RedBusApplication

--- index.js
+++ index.js
@@ -36,9 +36,9 @@ app.use("/booking",(Bookings))
 
 
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-    app.use(express.static('../client/build'));
+    app.use(express.static('./client/build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname + '../client/build/index.html'));
+        res.sendFile(path.join(__dirname + './client/build/index.html'));
     });
 }
 // app.get('*', (req, res) => {
