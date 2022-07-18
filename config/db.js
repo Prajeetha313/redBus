e5f40e49098494cc3214fc31ce937cbc17866f81
Prajeetha R <82923980+Prajeetha313@users.noreply.github.com>
2022-07-18 19:53:37 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 var mongoose=require('mongoose');
 require('dotenv').config();
 mongoose
-     .connect(process.env.DB || 'mongodb+srv://PrajeethaRaj:Corona-19@cluster0.08uhr.mongodb.net/RedBus?retryWrites=true&w=majority', 
+     .connect(process.env.DB, 
      { useNewUrlParser: true, useUnifiedTopology: true })
      .then(() => console.log('Database Connected',mongoose.connection.host,mongoose.connection.db))
      .catch(err => console.log(err));
